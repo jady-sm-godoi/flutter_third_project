56a226ea01930552f5c571b5fb96874410f018b2
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-03-08 20:35:30 -0300

    filter favorite global way with provider

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -4,8 +4,24 @@ import 'package:udemy_shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   List<Product> _items = dummyProducts;
+  bool _showFavorite = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavorite) {
+      return _items.where((product) => product.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavorite() {
+    _showFavorite = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavorite = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:udemy_shop/models/product_list.dart';
 import '../components/product_grid.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -13,8 +22,29 @@ class ProductsOverviewPage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.menu),
+            itemBuilder: (_) => const [
+              PopupMenuItem(
+                child: Text('apenas os favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('todos os produtos'),
+                value: FilterOptions.All,
+              )
+            ],
+            onSelected: (FilterOptions value) => {
+              if (value == FilterOptions.Favorite)
+                {provider.showFavorite()}
+              else
+                {provider.showAll()}
+            },
+          )
+        ],
       ),
-      body: ProductGrid(),
+      body: const ProductGrid(),
     );
   }
 }
