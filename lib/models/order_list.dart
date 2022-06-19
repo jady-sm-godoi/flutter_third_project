92fef8c2a0e0141fb2a76d2d8450a6a4732a5c8f
jady-sm-godoi <jady.s.m@gmail.com>
2022-06-19 15:37:21 -0300

    loadOrders from firebase

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 import 'package:udemy_shop/models/cart.dart';
+import 'package:udemy_shop/models/cart_item.dart';
 import 'package:udemy_shop/models/order.dart';
 import 'package:udemy_shop/utils/constants.dart';
 
@@ -16,6 +18,34 @@ class OrderList extends ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response = await http.get(
+      Uri.parse('${Constants.ORDERS_BASE_URL}.json'),
+    );
+    if (response.body == 'null') {
+      return;
+    }
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((ordertId, orderData) {
+      _items.add(Order(
+        id: ordertId,
+        total: orderData['total'],
+        date: DateTime.parse(orderData['date']),
+        products: (orderData['products'] as List<dynamic>)
+            .map((item) => CartItem(
+                  id: item['id'],
+                  productId: item['productId'],
+                  name: item['name'],
+                  quantity: item['quantity'],
+                  price: item['price'],
+                ))
+            .toList(),
+      ));
+    });
+    notifyListeners();
   }
 
   Future<void> addOrder(Cart cart) async {
@@ -47,5 +77,12 @@ class OrderList extends ChangeNotifier {
       ),
     );
     notifyListeners();
+  }
+
+  Future<void> refreshOrders(BuildContext context) {
+    return Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders();
   }
 }
--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,8 +4,26 @@ import 'package:udemy_shop/components/app_drawer.dart';
 import 'package:udemy_shop/components/order_card.dart';
 import 'package:udemy_shop/models/order_list.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders().then((value) {
+      setState(() => _isLoading = false);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +32,19 @@ class OrdersPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Meus pedidos'),
       ),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderCard(order: orders.items[i]),
+      body: RefreshIndicator(
+        child: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) => OrderCard(order: orders.items[i]),
+              ),
+        onRefresh: () => Provider.of<OrderList>(
+          context,
+          listen: false,
+        ).refreshOrders(context),
       ),
       drawer: const AppDrawer(),
     );
