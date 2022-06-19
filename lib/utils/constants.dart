e79aa21d21fb25de190325e3336015b258f24117
jady-sm-godoi <jady.s.m@gmail.com>
2022-06-19 11:37:54 -0300

    orders to firebase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,7 @@
 class Constants {
   static const PRODUCT_BASE_URL =
       'https://shop-udemy-8ce2d-default-rtdb.firebaseio.com/products';
+
+  static const ORDERS_BASE_URL =
+      'https://shop-udemy-8ce2d-default-rtdb.firebaseio.com/orders';
 }
